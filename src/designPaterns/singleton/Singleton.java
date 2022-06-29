b3fc7b064e1843d8ce8b23af4490c36e685741f4
loredanat219 <loredanat219@gmail.com>
2022-06-29 18:50:24 +0300

    Design Pattern

--- src/designPaterns/singleton/Singleton.java
+++ src/designPaterns/singleton/Singleton.java
@@ -1,16 +1,22 @@
 package designPaterns.singleton;
 
 public class Singleton {
-    private static  Singleton instance;
-    public  static  int nrOfInstances;
+    private static Singleton instance;
+    public static int nrOfInstances;
 
-    private Singleton(){
+    private Singleton() {
         System.out.println("Din constr privat Singleton");
         nrOfInstances++;
     }
-    public  static  Singleton getInstance(){
-        if(instance==null){
-            instance= new Singleton();
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized ((Singleton.class)) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+
         }
         return instance;
     }
